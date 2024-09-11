726d8687b0d186af4360ce7f01c6aac9b5d9b03d
Batata-86 <felipebatata86@gmail.com>
2024-09-11 16:39:25 -0300

    Atualização

---
+++ contador/contador.js
@@ -0,0 +1,8 @@
+
+let contador = 0;
+
+while (contador <= 9) {
+    console.log(contador);
+    contador++;
+}
+alert(`Contador ${contador}`)
--- logica-js-aula_1/app.js
+++ logica-js-aula_1/app.js
@@ -1,12 +1,30 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
-console.log(chute)
-
-//se chute for igual a número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-}else{
-    alert('Voce errou de novo!')
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute
+console.log(chute);
+let tentativas = 1 
+//enquanto chute não for igual a numero secreto 
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+            //se chute for igual a número secreto
+        if (chute == numeroSecreto) {
+            break;
+            }else{
+                if (chute > numeroSecreto){
+                    alert (`o numero secreto é menor que ${chute}`);
+                }else {
+                    alert(`o numero secreto é maior que ${chute}`);
+                }
+                    //tentativas = tentativas + 1;
+                    tentativas++
+                 }
 }
+
+if(tentativas = 1) {
+   alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+
+}else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+}
+
